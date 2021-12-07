ed2c3e3f08a96fb8ce6ddaa3cb175bf79a044968
jadelcruz1 <jardelcruz6@gmail.com>
2021-12-07 17:22:53 -0300

    pagina projeto iniciada

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,13 @@
-
+import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
 
 function NewProject(){
 
   return(
-      <div> 
-          <h1>NewProject</h1>
-          <p> Conteúdo da página</p>
-          <p> Conteúdo da página</p>
+      <div className={styles.newproject_container }> 
+          <h1>Criar Projeto</h1>
+          <p> Crie seu projeto para depois adicionar os serviços </p>
+          <ProjectForm />
 
 
       </div>
