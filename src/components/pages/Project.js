62c2a993437af51499d7b66027cf3ea252209cdb
jadelcruz1 <jardelcruz6@gmail.com>
2022-01-30 21:50:46 -0300

    criando area de servico

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -19,6 +19,8 @@ function Project(){
 
     const [showProjectForm, setShowProjectForm] = useState(false)
 
+    const [showServiceForm, setShowServiceForm] = useState(false)
+
     const [message, setMessage] = useState()
 
     const [type, setType] = useState()
@@ -44,6 +46,8 @@ function Project(){
          }, [id])
 
          function editPost(project){
+                setMessage('')
+
              //budget validation
              if (project.budget < project.cost){
                  setMessage('O orçamento não pode ser menor que o custo do projeto!')
@@ -77,7 +81,9 @@ function Project(){
             setShowProjectForm(!showProjectForm)
          }
         
-
+         function toggleServiceForm() {
+            setShowServiceForm(!showServiceForm)
+         }
 
     return(
         <>
@@ -115,6 +121,21 @@ function Project(){
 
                              )}
                         </div>
+                        <div className={styles.service_form_container}>
+
+                            <h2>Adicione um serviço: </h2>
+
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                  { !showServiceForm ? 'Adicionar serviços' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm && <div> Formulário do serviço </div>}
+                            </div>
+                        </div>
+                        <h2> Serviços</h2>
+                        <Container customClass="start">
+                            <p>Itens de serviços</p>
+                        </Container>
 
                     </Container>
 
